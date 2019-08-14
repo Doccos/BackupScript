502ff1f181fb59e9ee05852baefbc139d6244e39
Doccos <git@adad.at>
2019-08-14 19:36:08 +0200

    changes

--- scripts/moveBackup.sh
+++ scripts/moveBackup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #DATAPATH="/var/backup"
-echo "$1 $2 $3 $4 $5 " > /var/backup/backupScript/log.log
+echo "$1 $2 $3 $4 $5 " > /opt/backupScript/log.log
 DATAPATH=$5
 if [ -n "$1" ] ; then
         NAME="$1"
