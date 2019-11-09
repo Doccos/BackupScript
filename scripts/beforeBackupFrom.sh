ca614b1a86e9d4f0ffbabe3236f779363096a41a
root <root@webcontrol.cc>
2019-11-09 10:54:40 +0100

    bugfix

--- scripts/beforeBackupFrom.sh
+++ scripts/beforeBackupFrom.sh
@@ -11,7 +11,7 @@ fi
 echo 0 > /opt/backupScript/tmp/$1
 
 if [ -f /opt/backupScript/scripts/executeBeforeFrom.sh ] ; then
-        /bin/bash /opt/backupScript/executeBeforeFrom.sh $BEZ
+        /bin/bash /opt/backupScript/scripts/executeBeforeFrom.sh $BEZ
 fi
 
 
